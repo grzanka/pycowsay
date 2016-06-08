3c9cd981c2b48febeec5369e63fc6e13e27df9aa
Leszek Grzanka <grzanka@agh.edu.pl>
2016-06-08 13:35:02 +0200

    comment improved

--- pycowsay/run_pycowsay.py
+++ pycowsay/run_pycowsay.py
@@ -12,7 +12,7 @@ def muu(verbose=False):
 
 def virus():
     print("Hi, I am an albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me other users. Many thanks for your cooperation! Best regards,Albanian virus\n")
-    print("As you can see, I am also very kind. To remove me, please run: \n pip uninstall pycowsay\n" )
+    print("As you can see, I am also very kind. To remove me, please run: \n sudo pip3 uninstall pycowsay\n and restart you shell session." )
     subprocess.call(["/bin/ls"] + sys.argv[1:])
 
 
